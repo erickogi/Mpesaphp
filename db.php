c6056024b9dca810d7f5543be27e1831a654f8b0
Eric kogi <erickogi14@gmail.com>
2018-04-06 23:28:22 +0300

    Update db.php

--- db.php
+++ db.php
@@ -3,10 +3,10 @@
  * Database configuration
  */
 
-define('DB_USERNAME', 'erickogi_erickog');
-define('DB_PASSWORD', '123ERYcog.');
+define('DB_USERNAME', '');
+define('DB_PASSWORD', '.');
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'erickogi_db');
+define('DB_NAME', '');
 
 
 
